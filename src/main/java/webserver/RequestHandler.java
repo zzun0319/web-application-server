d002e706e9fe3291278b3ab9b2b898a1203b82ea
zzun0319 <zzun0319@naver.com>
2022-02-06 15:59:16 +0900

    요구사항3까지 책보고 수정. 이해 완료.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,6 +33,7 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        	
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
         	
         		// 요청 정보 출력
@@ -42,36 +44,48 @@ public class RequestHandler extends Thread {
 	    		if(line == null) return;
 	    		
 	    		String[] tokens = line.split(" ");
+	    		int contentLength = 0;
 	    		
 	    		while(!line.equals("")) {
-	    			line = br.readLine();
 	    			log.debug("header: {}", line);
+	    			line = br.readLine();
+	    			if(line.contains("Content-Length")) {
+	    				contentLength = getContentLength(line);
+	    			}
 	    		}
+	    		// 여기까지 br이 ""까지 읽음. Header와 Body사이의 ""
+	    		// 그럼 다음 readLine을 하게되면 "" 아래의 Request Body를 읽게 됨. 또는 Content-Length만큼 읽어들이면 Body를 읽을 수 있음.
 	    		
 	    		String url = tokens[1];
-	    		if(url.startsWith("/user/create")){
-	    			int index = url.indexOf("?");
-	    			String queryString = url.substring(index + 1);
-	    			Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+	    		if("/user/create".equals(url)){
+//	    			int index = url.indexOf("?");
+//	    			String queryString = url.substring(index + 1);
+	    			
+	    			String body = IOUtils.readData(br, contentLength);
+	    			
+	    			Map<String, String> params = HttpRequestUtils.parseQueryString(body);
 	    			User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
 	    			log.debug("User : {}", user);
 	    			
-	    			DataOutputStream dos = new DataOutputStream(out);
-	    			byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
-	    			response200Header(dos, body.length);
-	    			responseBody(dos, body);
+	    			url = "/index.html";
 	    			
-	    		} else {
-	    			DataOutputStream dos = new DataOutputStream(out);
-	    			byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
-	    			response200Header(dos, body.length);
-	    			responseBody(dos, body);
-	    		}
+	    		} 
+	    		
+    			DataOutputStream dos = new DataOutputStream(out);
+    			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+    			response200Header(dos, body.length);
+    			responseBody(dos, body);
+	    		
 	    		
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+
+	private int getContentLength(String line) {
+		String[] headerTokens = line.split(":");
+		return Integer.parseInt(headerTokens[1].trim());
+	}
 
 	private String extractBodyFromReq(InputStream in, String requestUrl) throws IOException {
 		String reqBody = "";
